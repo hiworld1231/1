41ec2082607fccf533a00ea423dc082ec7d1d54e
hiworld1231 <shakirov.kir2@gmail.com>
2025-08-26 19:10:30 +0300

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,23 +1,21 @@
 -- Локальный скрипт
 local Players = game:GetService("Players")
-local ProximityPromptService = game:GetService("ProximityPromptService")
-local RunService = game:GetService("RunService")
-
 local player = Players.LocalPlayer
-local radius = 212
+local radius = 90
 
-RunService.RenderStepped:Connect(function()
+while true do
     local root = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
-    if not root then return end
-
-    for _, poster in ipairs(workspace:GetDescendants()) do
-        if poster.Name == "PosterPrompt" and poster:IsA("ProximityPrompt") then
-            local part = poster.Parent
-            if part and part:IsA("BasePart") and part.Parent and part.Parent.Name == "propogandaPoster" then
-                if (part.Position - root.Position).Magnitude <= radius then
-                    fireproximityprompt(poster, math.huge)
+    if root then
+        for _, poster in ipairs(workspace:GetDescendants()) do
+            if poster.Name == "PosterPrompt" and poster:IsA("ProximityPrompt") then
+                local part = poster.Parent
+                if part and part:IsA("BasePart") and part.Parent and part.Parent.Name == "propogandaPoster" then
+                    if (part.Position - root.Position).Magnitude <= radius then
+                        fireproximityprompt(poster, math.huge)
+                    end
                 end
             end
         end
     end
-end)
+    task.wait(0.000000000001)
+end
